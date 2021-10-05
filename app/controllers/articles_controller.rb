d80a13f442854f1ef327aae7da59bb5633508b1a
sathwik pk <sathwik.intern@latlong.in>
2021-10-05 14:39:12 +0530

    creating first commit on orphan branch to delete history

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:desc))
+    if res = @article.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article
+    else
+        render 'new'
+    end
+    # render plain: @article.inspect
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:desc))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+end
