4fc16859cfbfafef0b839c281d8204f88974e2f2
sathwik4 <sathwik@coffeebeans.io>
2023-05-12 16:08:17 +0530

    add FE overhaul

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 6, maximum: 50}
-  validates :description, presence: true, length: {maximum: 200}
+  validates :description, presence: true, length: {minimum: 20, maximum: 200}
 end
