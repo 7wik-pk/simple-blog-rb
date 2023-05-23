0868df1c00abf466d8333a368ad522e38128c009
sathwik4 <sathwik@coffeebeans.io>
2023-05-23 14:55:29 +0530

    add user signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#home"
 
   get '/about', to: 'pages#about'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   resources :articles
 end
