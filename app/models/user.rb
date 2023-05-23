bcd034eb4f5bacc4886eb6c4a1debeba97349988
sathwik4 <sathwik@coffeebeans.io>
2023-05-23 12:07:13 +0530

    add one-to-many association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_many :articles
+
   validates :username, presence: true,
                       uniqueness: {case_sensitive: false},
                       length: {minimum: 3, maximum: 25}
@@ -10,5 +12,6 @@ class User < ApplicationRecord
                   uniqueness: {case_sensitive: false},
                   length: {maximum: 105},
                   format: {with: VALID_EMAIL_REGEX}
+
 
 end
